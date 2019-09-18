38571d9f5391c2ef72cb27524596be59eb95388b
Rafael Cruz <rafael.cruz@iscoolapp.com.br>
2019-09-18 16:14:23 -0300

    Migrated to @claviska/jquery-minicolors

--- angular-minicolors.js
+++ angular-minicolors.js
@@ -3,9 +3,9 @@
 
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['angular', 'jquery-minicolors'], factory);
+    define(['angular', '@claviska/jquery-minicolors'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('angular'), require('jquery-minicolors'));
+    module.exports = factory(require('angular'), require('@claviska/jquery-minicolors'));
     module.exports = 'minicolors';
   } else {
     root.angularMinicolors = factory(root.angular, root.jqueryMinicolors);
@@ -30,7 +30,10 @@
       letterCase: 'lowercase',
       opacity: false,
       show: null,
-      showSpeed: 100
+      showSpeed: 100,
+      format: 'hex',
+      keywords: '',
+      swatches: []
     };
 
     this.$get = function () {
@@ -46,11 +49,11 @@
       priority: 1, //since we bind on an input element, we have to set a higher priority than angular-default input
       link: function (scope, element, attrs, ngModel) {
 
-        var inititalized = false;
+        let initialized = false;
 
         //gets the settings object
-        var getSettings = function () {
-          var config = angular.extend({}, minicolors.defaults, scope.$eval(attrs.minicolors));
+        const getSettings = function () {
+          const config = angular.extend({}, minicolors.defaults, scope.$eval(attrs.minicolors));
           return config;
         };
 
@@ -59,16 +62,42 @@
          * e.g.#fff000 or #fff
          * @param color
          */
+        function isHEX(color, testShortHex) {
+          return (testShortHex && /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color)) ||
+            /^#([A-Fa-f0-9]{6})$/.test(color);
+        }
+
+        function isRGB(color) {
+          const rgb = (color || '').match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+          return (rgb && rgb.length === 4) ? true : false;
+        }
+
         function isValidColor(color) {
-          return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color);
+          return isHEX(color) || isRGB(color);
         }
 
         function canSetValue() {
-          return (element.data('minicolors-settings') != null)
+          return (element.data('minicolors-settings') !== null);
         }
 
         function isEmpty(value) {
           return value === null || value === undefined || String(value).trim() === '';
+        }
+
+        function fixCase(value) {
+
+          if (!value) return null;
+
+          switch ((getSettings().letterCase || '').toLowerCase()) {
+            case 'uppercase':
+              value = value.toUpperCase();
+              break;
+            case 'lowercase':
+              value = value.toLowerCase();
+              break;
+          }
+
+          return value;
         }
 
         /**
@@ -79,35 +108,50 @@
 
           if (!canSetValue()) return;
 
-          if (isEmpty(color)) {
-            element.minicolors('value', null);
-          } else if (isValidColor(color)) {
-            element.minicolors('value', color);
+          if (isHEX(color)) {
+            element.minicolors('value', {color: sanitizeColor(color), opacity: 1});
+          } else {
+            element.minicolors('value', sanitizeColor(color));
           }
+        }
+
+        function sanitizeColor(color) {
+
+          console.log('color -> ' + color);
+
+          if (isEmpty(color) || !isValidColor(color)) {
+            console.log('invalid color -> ' + color);
+            return null;
+          }
+
+          color = fixCase(color);
+          console.log('sanitizedColor -> ' + color);
+          return color;
         }
 
         //what to do if the value changed
         ngModel.$render = function () {
 
-
           //we are in digest or apply, and therefore call a timeout function
           $timeout(function () {
-            var color = ngModel.$viewValue;
-            setMinicolorsValue(color);
+            const color = ngModel.$viewValue;
+            if (isValidColor(color)) {
+              setMinicolorsValue(color);
+            }
           }, 0, false);
         };
 
         //init method
-        var initMinicolors = function () {
+        const initMinicolors = function () {
 
           if (!ngModel) {
             return;
           }
-          var settings = getSettings();
-          settings.change = function (hex) {
+          const settings = getSettings();
+          settings.change = function (color, opacity) {
             scope.$apply(function () {
-              if (isValidColor(hex))
-                ngModel.$setViewValue(hex);
+              console.log('change -> ' + color);
+              ngModel.$setViewValue(sanitizeColor(color));
             });
           };
 
@@ -123,32 +167,33 @@
           // hook up into the jquery-minicolors onBlur event.
           element.on('blur', onBlur);
 
-          // are we inititalized yet ?
+          // are we initialized yet ?
           //needs to be wrapped in $timeout, to prevent $apply / $digest errors
           //$scope.$apply will be called by $timeout, so we don't have to handle that case
-          if (!inititalized) {
+          if (!initialized) {
             $timeout(function () {
-              var color = ngModel.$viewValue;
+              const color = ngModel.$viewValue;
               setMinicolorsValue(color);
             }, 0);
-            inititalized = true;
+            initialized = true;
             return;
           }
 
           function onBlur(e) {
             scope.$apply(function () {
-              var color = element.minicolors('value');
-              if (isValidColor(color))
-                ngModel.$setViewValue(color);
+              const color = sanitizeColor(element.minicolors('value'));
+              console.log('blur -> ' + color);
+              ngModel.$setViewValue(color);
+              setMinicolorsValue(color);
             });
           }
+
         };
 
         initMinicolors();
-        //initital call
 
         // Watch for changes to the directives options and then call init method again
-        var unbindWatch = scope.$watch(getSettings, initMinicolors, true);
+        const unbindWatch = scope.$watch(getSettings, initMinicolors, true);
 
         scope.$on('$destroy', function () {
           if (element.hasClass('minicolors-input')) {
